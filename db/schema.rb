5b646344fd299bfbcceb5241b68976dc40fa36f1
yana-gi <yanagisawa.hika@gmail.com>
2020-12-02 15:32:15 +0900

    ユーザーモデルのカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_22_011641) do
+ActiveRecord::Schema.define(version: 2020_12_02_085558) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -33,8 +33,11 @@ ActiveRecord::Schema.define(version: 2020_11_22_011641) do
     t.string "postal_code"
     t.string "address"
     t.text "self_introduction"
+    t.string "uid"
+    t.string "provider", default: "", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
 end
