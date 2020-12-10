93f10a0881c3edd58af4007620602fd1a902b1d7
yana-gi <yanagisawa.hika@gmail.com>
2020-12-10 17:24:47 +0900

    プロフィール更新時のパスワード入力を不要にする

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   devise_for :users,controllers: {
+    registrations: 'users/registrations',
     omniauth_callbacks: "users/omniauth_callbacks"
   }
   resources :books
