84111d7385aafdcf8564c6fc9b7928ef9e35d6f2
yana-gi <yanagisawa.hika@gmail.com>
2020-12-12 17:21:22 +0900

    レビュー修正 rubocop指摘内容修正

--- db/migrate/20201202085558_add_columns_to_users.rb
+++ db/migrate/20201202085558_add_columns_to_users.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 class AddColumnsToUsers < ActiveRecord::Migration[6.0]
   def change
     add_column :users, :uid, :string
-    add_column :users, :provider, :string, null: false, default: ""
+    add_column :users, :provider, :string, null: false, default: ''
     add_index :users, %i[uid provider], unique: true
   end
 end
