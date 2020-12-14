24186c19d8eeec6800374f9dfe004438bb91abd0
yana-gi <yanagisawa.hika@gmail.com>
2020-12-14 18:55:14 +0900

    レビュー修正 Userにバリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: [:github]
 
+  validates :uid, uniqueness: { scope: :provider }
+
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create! do |user|
       user.name = auth.info.name
