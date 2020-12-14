5a238d8e76bcdb132bb6e0615fd22bc9d75736a6
yana-gi <yanagisawa.hika@gmail.com>
2020-12-14 17:47:39 +0900

    レビュー修正 保存失敗時の処理修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
   validates :uid, uniqueness: { scope: :provider }
 
   def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create! do |user|
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.name = auth.info.name
       user.email = auth.info.email
       user.password = Devise.friendly_token[0, 20]
