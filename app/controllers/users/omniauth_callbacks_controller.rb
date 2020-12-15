a94f500447752fb69a8978e1ed007df03bae85f4
yana-gi <yanagisawa.hika@gmail.com>
2020-12-15 18:45:09 +0900

    レビュー修正 is_navigational_format?判定削除

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -6,7 +6,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
     if @user.persisted?
       sign_in_and_redirect @user, event: :authentication
-      set_flash_message(:notice, :success, kind: 'GitHub') if is_navigational_format?
+      set_flash_message(:notice, :success, kind: 'GitHub')
     else
       redirect_to new_user_registration_url
     end
