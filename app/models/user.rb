a0e78f3a6107762acba30d41549585e7fbdbf922
yana-gi <yanagisawa.hika@gmail.com>
2020-12-15 19:24:13 +0900

    レビュー修正 バリデーションエラー修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: [:github]
 
-  validates :uid, uniqueness: { scope: :provider }
+  validates :uid, uniqueness: { scope: :provider }, unless: -> { uid.blank? }
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
