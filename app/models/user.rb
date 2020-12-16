ee438360ae3eff7ee5668e0b864f60222c18a218
yana-gi <yanagisawa.hika@gmail.com>
2020-12-16 12:08:51 +0900

    レビュー修正 バリデーションのオプションをifに修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: [:github]
 
-  validates :uid, uniqueness: { scope: :provider }, unless: -> { uid.blank? }
+  validates :uid, uniqueness: { scope: :provider }, if: -> { uid.present? }
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
