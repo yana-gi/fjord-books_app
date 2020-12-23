689ad8afc65604c1e35655ab634dee3088685225
yana-gi <yanagisawa.hika@gmail.com>
2020-12-23 14:01:31 +0900

    フォロー機能追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,14 @@ class User < ApplicationRecord
          :omniauthable, omniauth_providers: %i[github]
 
   has_one_attached :avatar
+  has_many :following_relationships, class_name: 'Relationship',
+                                     foreign_key: 'follower_id',
+                                     dependent: :destroy
+  has_many :follower_relationships, class_name: 'Relationship',
+                                    foreign_key: 'followed_id',
+                                    dependent: :destroy
+  has_many :following, through: :following_relationships, source: :followed
+  has_many :followers, through: :follower_relationships, source: :follower
 
   validates :uid, uniqueness: { scope: :provider }, if: -> { uid.present? }
 
@@ -15,5 +23,17 @@ class User < ApplicationRecord
       user.email = auth.info.email
       user.password = Devise.friendly_token[0, 20]
     end
+  end
+
+  def follow(other_user)
+    following << other_user
+  end
+
+  def unfollow(other_user)
+    following_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
   end
 end
