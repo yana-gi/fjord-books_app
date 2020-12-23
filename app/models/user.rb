d1859006ed0bfe8bd78d1ed809ac8728dfd80930
yana-gi <yanagisawa.hika@gmail.com>
2020-12-23 17:36:47 +0900

    inverse_of追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,12 @@ class User < ApplicationRecord
   has_one_attached :avatar
   has_many :following_relationships, class_name: 'Relationship',
                                      foreign_key: 'follower_id',
-                                     dependent: :destroy
+                                     dependent: :destroy,
+                                     inverse_of: :follower
   has_many :follower_relationships, class_name: 'Relationship',
                                     foreign_key: 'followed_id',
-                                    dependent: :destroy
+                                    dependent: :destroy,
+                                    inverse_of: :followed
   has_many :following, through: :following_relationships, source: :followed
   has_many :followers, through: :follower_relationships, source: :follower
 
