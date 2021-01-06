10f47f5f747418b795f9f2952ab5f1f3e50d7f9b
yana-gi <yanagisawa.hika@gmail.com>
2021-01-06 13:48:25 +0900

    レビュー修正 Unfollow処理リファクタリング

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -8,7 +8,7 @@ class RelationshipsController < ApplicationController
   end
 
   def destroy
-    @user = Relationship.find(params[:id]).followed
+    @user = User.find(params[:id])
     current_user.unfollow(@user)
     redirect_to @user
   end
