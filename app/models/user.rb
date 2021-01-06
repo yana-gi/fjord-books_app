c418198ab45a0e14cc2284143908bc26c477fd04
yana-gi <yanagisawa.hika@gmail.com>
2021-01-06 14:34:08 +0900

    レビュー修正 user.rb のunfollow処理を destroy から destroy! へ修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -32,7 +32,7 @@ class User < ApplicationRecord
   end
 
   def unfollow(other_user)
-    following_relationships.find_by(followed_id: other_user.id).destroy
+    following_relationships.find_by(followed_id: other_user.id).destroy!
   end
 
   def following?(other_user)
