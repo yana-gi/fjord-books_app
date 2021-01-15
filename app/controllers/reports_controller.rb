8ec4dfb315d6215887d084cb5c5effa461b4e0d7
yana-gi <yanagisawa.hika@gmail.com>
2021-01-15 17:14:52 +0900

    reports（日報）作成

---
+++ app/controllers/reports_controller.rb
@@ -0,0 +1,68 @@
+class ReportsController < ApplicationController
+  before_action :set_report, only: [:show, :edit, :update, :destroy]
+
+  # GET /reports
+  # GET /reports.json
+  def index
+    @reports = Report.all
+  end
+
+  # GET /reports/1
+  # GET /reports/1.json
+  def show
+  end
+
+  # GET /reports/new
+  def new
+    @report = Report.new
+  end
+
+  # GET /reports/1/edit
+  def edit
+  end
+
+  # POST /reports
+  def create
+    @report = Report.new(report_params)
+
+    respond_to do |format|
+      if @report.save
+        format.html { redirect_to @report, notice: 'Report was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  # PATCH/PUT /reports/1
+  def update
+    respond_to do |format|
+      if @report.update(report_params)
+        format.html { redirect_to @report, notice: 'Report was successfully updated.' }
+       else
+        format.html { render :edit }
+       end
+    end
+  end
+
+  # DELETE /reports/1
+  # DELETE /reports/1.json
+  def destroy
+    @report.destroy
+    respond_to do |format|
+      format.html { redirect_to reports_url, notice: 'Report was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_report
+      @report = Report.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def report_params
+      params.require(:report).permit(:title, :text)
+    end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_22_114857) do
+ActiveRecord::Schema.define(version: 2021_01_15_080730) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -49,6 +49,13 @@ ActiveRecord::Schema.define(version: 2020_11_22_114857) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["follower_id", "following_id"], name: "index_relationships_on_follower_id_and_following_id", unique: true
     t.index ["following_id"], name: "index_relationships_on_following_id"
+  end
+
+  create_table "reports", force: :cascade do |t|
+    t.string "title"
+    t.text "text"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
