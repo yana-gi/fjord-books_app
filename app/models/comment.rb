478eb595a9bd440919935b967a6e15731edb4ac4
yana-gi <yanagisawa.hika@gmail.com>
2021-01-19 15:42:23 +0900

    ポリモーフィック 関連付け

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,4 @@
 class Comment < ApplicationRecord
   belongs_to :user
-  belongs_to :report
+  belongs_to :commentable, polymorphic: true
 end
--- app/models/report.rb
+++ app/models/report.rb
@@ -1,4 +1,4 @@
 class Report < ApplicationRecord
   belongs_to :user
-  has_many :comments, dependent: :destroy
+  has_many :comments, as: :commentable
 end
